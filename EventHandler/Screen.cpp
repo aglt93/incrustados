2c28a99bd244fd495a4b3031db7b3316bf5e2824
kvallecillog <kvallecillog@gmail.com>
2016-11-17 01:39:04 -0600

    Screen comments

--- EventHandler/Screen.cpp
+++ EventHandler/Screen.cpp
@@ -1,32 +1,33 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
 #define __NOP __nop
-
+//////////////////////////////////////////////////////////////////////////////////////////////
 extern "C"
 {
 	#include <driverlib.h>
 	#include <grlib.h>
 	#include "Crystalfontz128x128_ST7735.h"
 }
-#include <stdio.h>
+//////////////////////////////////////////////////////////////////////////////////////////////
 #include "Screen.hpp"
 #include "Task.hpp"
 #include "task_ids.hpp"
-
-#define INITIAL_COND_F 0
-#define FINAL_COND_F 64
+//////////////////////////////////////////////////////////////////////////////////////////////
 #define FIRST_PIXEL 0
 #define LAST_PIXEL 127
+//////////////////////////////////////////////////////////////////////////////////////////////
 /* Graphic library context */
 Graphics_Context g_sContext;
+//////////////////////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * Constructor de la pantalla para configuraciones iniciales.
+ */
+//////////////////////////////////////////////////////////////////////////////////////////////
 Screen::Screen(int i_iTaskID, bool i_bPeriodicTask)
 {
 	m_iTaskID = i_iTaskID;
 	m_bPeriodicTask = i_bPeriodicTask;
-
-	m_u64CurrentCount = 0;
-	m_u64FinalCount = 1000;
-    m_u16Initial = 63;
-    m_ys = 0;
 
 	//ctor
 	Graphics_Context g_sContext;
@@ -47,85 +48,28 @@ Screen::Screen(int i_iTaskID, bool i_bPeriodicTask)
 	GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
 	Graphics_clearDisplay(&g_sContext);
 
-	//Screen::printScreen(0,63,5000);
-
-//	for ( m_ys=m_u16Initial; m_ys<128; m_ys++ ){
-//		Graphics_drawLineH(&g_sContext, 0, 127, m_ys);
-//	}
-
 }
+//////////////////////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////////////////////////////
 uint8_t Screen::run(void)
 {
-    //#########################
-    // Blink code
-    //#########################
-    //#########################
-    // Blink code
-    //#########################
-
-//	m_u16Initial
-
-	//m_iTaskID = i_iTaskID;
-	//	m_bPeriodicTask = i_bPeriodicTask;
-//
-//		m_u64CurrentCount = 0;
-//		m_u64FinalCount = 1000;
-//	    m_u16Initial = 30;
-//	    m_ys = 0;
-//
-//		//ctor
-//		Graphics_Context g_sContext;
-//
-//	    /* Initializes display */
-//		Crystalfontz128x128_Init();
-//
-//		/* Set default screen orientation */
-//		Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_DOWN);
-//
-//		/* Initializes graphics context */
-//		Graphics_initContext(&g_sContext, &g_sCrystalfontz128x128);
-//	    GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
-//
-//		/* Draw Title, x-axis, gradation & labels */
-//		Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_SANDY_BROWN);
-//		Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_STEEL_BLUE);
-//		GrContextFontSet(&g_sContext, &g_sFontFixed6x8);
-//		Graphics_clearDisplay(&g_sContext);
-//
-//		//Screen::printScreen(0,63,5000);
-//
-//	//	for ( m_ys=m_u16Initial; m_ys<128; m_ys++ ){
-//	//		Graphics_drawLineH(&g_sContext, 0, 127, m_ys);
-//	//	}
-//
-////
-//		for ( m_ys=0; m_ys<70; m_ys++ ){
-//		Graphics_drawLineH(&g_sContext, 0, 127, m_ys);
-//	}
-
     return(NO_ERR);
 }
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * Esta funcion de referencia los punteros para obtener los valores del acelerometro
+ * y ser procedados por los metodos para imprimir la pantalla.
+ */
+//////////////////////////////////////////////////////////////////////////////////////////////
 void Screen::ProcessMessage(MSG i_Message) {
 
-	//int l_iSourceTask = i_Message.source;
+	/* Se extrae la referencia del puntero al vector de datos del acelerometro. */
 	int* l_pDataTask = (int*) i_Message.data;
 
-	//printScreen(0,*l_pDataTask,1);
-	//m_u16Initial = *l_pDataTask;
-
-
-	m_u64CurrentCount = 0;
-	m_u64FinalCount = 1000;
-    m_ys = 0;
-
-//	uint8_t y = 0;
-//	uint16_t x = *(l_pDataTask+1);
-//	m_u16Initial = (x-5000)/51.8;
-//	ADCtoScreenValueConv()
-
+	/* Se convierte el valor del acelerometro en lineas para la pantalla. */
     int l_iScreenValue = ADCtoScreenValueConv(*(l_pDataTask+1));
 
 	/* Initializes graphics context */
@@ -136,100 +80,149 @@ void Screen::ProcessMessage(MSG i_Message) {
 	Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_SANDY_BROWN);
 	Graphics_setBackgroundColor(&g_sContext, GRAPHICS_COLOR_STEEL_BLUE);
 
-/*
-	for ( m_ys=FIRST_PIXEL; m_ys<LAST_PIXEL+1; m_ys++ ){
-
-		if(m_ys<m_u16Initial+1){
-			Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_SANDY_BROWN );
-			Graphics_drawLineH(&g_sContext, 0, 127, m_ys);
-		}
-		else
-			Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_STEEL_BLUE );
-			Graphics_drawLineH(&g_sContext, 0, 127, m_ys);
-
-	}
-*/
+	/* Se ejecuta la funcion que actualiza las lineas en la pantalla. */
 	printScreen(0,l_iScreenValue,*(l_pDataTask));
 
 }
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////
 /*
- * Clear display and redraw title + accelerometer data
+ * Esta funcion actualiza las lineas de la patanlla conforme el acelerometro detecta cambios.
+ * Los datos del acelerometro se traducen a coordenadas en el eje y de la pantalla.
  */
+//////////////////////////////////////////////////////////////////////////////////////////////
 void Screen::printScreen(uint8_t l_u16Initial, uint8_t l_u16Final, uint16_t l_u8UpOrDown)
 {
+
 	/* 0 0 0 grad All GRAPHICS_COLOR_SANDY_BROWN
-	 0 64 45 grad Half GRAPHICS_COLOR_SANDY_BROWN Half GRAPHICS_COLOR_STEEL_BLUE
-	 0 127 90 grad All GRAPHICS_COLOR_STEEL_BLUE
+	   0 64 45 grad Half GRAPHICS_COLOR_SANDY_BROWN Half GRAPHICS_COLOR_STEEL_BLUE
+	   0 127 90 grad All GRAPHICS_COLOR_STEEL_BLUE
 	*/
-	uint8_t ys = 0;
+
+	uint8_t l_u8Ys = 0;
+
+	/* Se utilizan los datos del acelerometro en el eje Y para determinar si
+	 * el dispositivo fue volteado hacia arriba.
+	 */
 
 	if (l_u8UpOrDown > 11300) {
+
+		/* Determina la configuracion actual de la pantalla.
+		 * Se actualiza solo si es diferente de la actualizacion.
+		 */
+
 		if (Lcd_Orientation != LCD_ORIENTATION_UP){
+
 			Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP);
+
 		}
 		else
-			 ys = 0;
 
-			for ( ys=FIRST_PIXEL; ys<LAST_PIXEL+1; ys++ ){
+			l_u8Ys = 0;
 
-				if(ys<l_u16Final+1){
+			for ( l_u8Ys=FIRST_PIXEL; l_u8Ys<LAST_PIXEL+1; l_u8Ys++ ){
+
+				if(l_u8Ys<l_u16Final+1){
+
+					/*
+					 * Imprime la tierra del horizonte
+					 */
 					Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_SANDY_BROWN );
-					Graphics_drawLineH(&g_sContext, 0, 127, ys);
+					Graphics_drawLineH(&g_sContext, 0, 127, l_u8Ys);
+
 				}
 				else
+
+					/*
+					 * Imprime las lineas pendientes del cielo del horizonte.
+					 */
 					Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_STEEL_BLUE );
-					Graphics_drawLineH(&g_sContext, 0, 127, ys);
+					Graphics_drawLineH(&g_sContext, 0, 127, l_u8Ys);
+
 
 			}
 	}
 
+	/* Se utilizan los datos del acelerometro en el eje Y para determinar si
+	 * el dispositivo fue volteado hacia abajo.
+	 */
+
 	else if (l_u8UpOrDown < 11300) {
+
 		if (Lcd_Orientation != LCD_ORIENTATION_DOWN){
+
 			Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_DOWN);
+
 		}
 		else
-			ys = 0;
 
-			for ( ys=FIRST_PIXEL; ys<LAST_PIXEL+1; ys++ ){
+			l_u8Ys = 0;
 
-				if(ys<l_u16Final+1){
+			for ( l_u8Ys=FIRST_PIXEL; l_u8Ys<LAST_PIXEL+1; l_u8Ys++ ){
+
+				if(l_u8Ys<l_u16Final+1){
+
+					/*
+					 * Imprime la tierra del horizonte
+					 */
 					Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_SANDY_BROWN );
-					Graphics_drawLineH(&g_sContext, 0, 127, ys);
+					Graphics_drawLineH(&g_sContext, 0, 127, l_u8Ys);
+
 				}
+
 				else
+
+					/*
+					 * Imprime las lineas pendientes del cielo del horizonte.
+					 */
 					Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_STEEL_BLUE );
-					Graphics_drawLineH(&g_sContext, 0, 127, ys);
+					Graphics_drawLineH(&g_sContext, 0, 127, l_u8Ys);
 
 			}
 	}
 
 	else
 
-		ys = 0;
+		l_u8Ys = 0;
 
-		for ( ys=FIRST_PIXEL; ys<LAST_PIXEL+1; ys++ ){
+		for ( l_u8Ys=FIRST_PIXEL; l_u8Ys<LAST_PIXEL+1; l_u8Ys++ ){
 
-			if(ys<l_u16Final+1){
+			if(l_u8Ys<l_u16Final+1){
+
+				/*
+				 * Imprime la tierra del horizonte
+				 */
 				Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_SANDY_BROWN );
-				Graphics_drawLineH(&g_sContext, 0, 127, ys);
+				Graphics_drawLineH(&g_sContext, 0, 127, l_u8Ys);
+
 			}
 			else
+
+				/*
+				 * Imprime las lineas pendientes del cielo del horizonte.
+				*/
 				Graphics_setForegroundColor(&g_sContext, GRAPHICS_COLOR_STEEL_BLUE );
-				Graphics_drawLineH(&g_sContext, 0, 127, ys);
+				Graphics_drawLineH(&g_sContext, 0, 127, l_u8Ys);
 
 		}
 
 }
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////
+/* Los datos del acelerometro se traducen a coordenadas en el eje y de la pantalla.
+ * por medio de una ecuacion lineal con offset.
+ */
+//////////////////////////////////////////////////////////////////////////////////////////////
 int Screen::ADCtoScreenValueConv(int i_iADCvalue){
 
 	int l_iServoValue;
 
+	/* Se transforma el valor del ADC a lineas de acuerdo a la ecuacion de una recta */
 	l_iServoValue = (float) (i_iADCvalue-5000)/51.8;
 
 	return l_iServoValue;
 
 }
+//////////////////////////////////////////////////////////////////////////////////////////////
